4a9ad2320a0e974acee54549db31ea45392883b1
dotsquares-support <support.extensions@dotsquares.com>
2021-08-10 18:47:39 +0530

    Uploading delete orders ext files

---
+++ DeleteOrders/DeleteOrders2.1.1/DeleteOrders/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Dotsquares Team
+ * @copyright Copyright (c) 2015 Dotsquares (http://www.dotsquares.com)
+ * @package Dotsquares_Deleteorders
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Dotsquares_DeleteOrders',
+    __DIR__
+);
